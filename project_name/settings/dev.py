8b87700175b46759a63a8093c24a16eb1703bf66
altnight <altnightsight@gmail.com>
2014-05-03 21:27:34 +0900

    add django-devserver

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -4,7 +4,6 @@ from {{ project_name }}.settings.base import *  # NOQA
 DEBUG = True
 TEMPLATE_DEBUG = True
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -12,14 +11,40 @@ DATABASES = {
     }
 }
 
+####################
+# debug toolbar
+####################
+
 INSTALLED_APPS += (
     'django.contrib.staticfiles',
     'debug_toolbar',
 )
-
 
 MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 STATIC_URL = '/static/'
+
+####################
+# devserver
+####################
+
+INSTALLED_APPS += (
+    'devserver',
+)
+
+MIDDLEWARE_CLASSES += (
+    'devserver.middleware.DevServerMiddleware',
+)
+
+DEVSERVER_MODULES = (
+    'devserver.modules.sql.SQLRealTimeModule',
+    'devserver.modules.sql.SQLSummaryModule',
+    'devserver.modules.profile.ProfileSummaryModule',
+
+    # 'devserver.modules.ajax.AjaxDumpModule',
+    # 'devserver.modules.profile.MemoryUseModule',
+    # 'devserver.modules.cache.CacheSummaryModule',
+    # 'devserver.modules.profile.LineProfilerModule',
+)
